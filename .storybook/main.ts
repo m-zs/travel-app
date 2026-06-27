4c05904e544fe5a45f5ef88fc6027a08af75f083
unknown <mzasada93@gmail.com>
2026-06-27 11:21:10 +0200

    chore: add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from "@storybook/nextjs-vite";
+import type { UserConfig } from "vite";
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-mcp",
+  ],
+  framework: "@storybook/nextjs-vite",
+  staticDirs: ["../public"],
+  async viteFinal(config: UserConfig) {
+    if (process.env.STORYBOOK_BASE_PATH) {
+      config.base = process.env.STORYBOOK_BASE_PATH;
+    }
+
+    return config;
+  },
+};
+
+export default config;
