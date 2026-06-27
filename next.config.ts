fcda6f8f566887e3a55b60d012420f65d7e4c008
unknown <mzasada93@gmail.com>
2026-06-27 11:07:17 +0200

    chore: dockerize

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isDockerDev = process.env.WATCHPACK_POLLING === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  allowedDevOrigins: ["127.0.0.1", "localhost"],
+  ...(isDockerDev && {
+    watchOptions: {
+      pollIntervalMs: 1000,
+    },
+  }),
 };
 
 export default nextConfig;
